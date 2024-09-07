25359da7aedbe7e170c2a200644139b05686c0e1
Novik-2000 <lyubasha.novikova.2000@mail.ru>
2024-09-08 00:40:17 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,9 @@
+amount_of_tasks = 12
+amount_of_hours = 1.5
+name_of_course = "Python"
+print (1.5 / 12)
+time_for_one_task = 0,125
+print("курс",name_of_course,"всего задач",amount_of_tasks,"затрачено часов", amount_of_hours, "среднее время выполнения", time_for_one_task )
+
+
+
